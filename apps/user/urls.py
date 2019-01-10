5d351e3322f5516f6352a9ca361827f216606b40
cdzm5211314 <cdzm5211314@163.com>
2019-01-10 15:26:46 +0800

    使用类视图完成用户的注册请求

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 
     url(r'^register$', views.register, name='register'),  # 显示注册页面  # 显示注册页面和处理注册用户请求
     # url(r'^register_handle$', views.register_handle, name='register_handle'),  # 显示注册页面
+    # url(r'^registerview$',views.RegisterView.as_view(),name='registerview'),  # 使用类视图处理用户注册请求
+
 
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,HttpResponse
 from django.core.urlresolvers import reverse
-
+from django.views.generic import View
 from apps.user.models import User
 import re
 
 
-# Create your views here.
 
 # /user/register
 def register(request):
@@ -90,9 +89,49 @@ def register_handle(request):
     return redirect(reverse('goods:index'))
 """
 
+# /user/registerview
+class RegisterView(View):
+    '''使用类视图显示注册页面和处理注册用户请求'''
+    def get(self,request):
+        # 显示注册页面
+        return render(request, 'register.html')
 
+    def post(self,request):
+        # 获取注册请求参数
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+        # print(allow)
 
+        # 进行数据校验
+        if not all([username, password, email]):
+            return render(request, 'register.html', {'errormessage': '数据不完整'})
 
+        # 校验邮箱
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errormessage': '邮箱格式不正确'})
+
+        # 校验是否同意协议
+        if allow != 'on':
+            return render(request, 'register.html', {'errormessage': '请同意协议'})
+
+        # 验证要注册的用户是否已经存在,get()当查不到数据时会抛出异常
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # 说明用户不存在
+            user = None
+        if user:  # 用户名已经存在
+            return render(request, 'register.html', {'errormessage': '用户名已经存在'})
+
+        # 业务处理,进行用户注册
+        user = User.objects.create_user(username, password, email)  # 默认是账号注册后是激活状态
+        user.is_active = 0  # 设置为不是激活状态
+        user.save()
+
+        # 返回应答,跳转到主页
+        return redirect(reverse('goods:index'))
 
 
 
