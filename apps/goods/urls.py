cdff5a6bfadbac106ff6d3bb08905c4876435da8
chendong <123309778@qq.com>
2019-09-11 15:14:58 +0800

    首页数据缓存的设置与获取

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -5,6 +5,6 @@ from apps.goods.views import IndexView
 urlpatterns = [
 
     # url(r'^$', views.index, name='index'),  # 显示主页
-    url(r'^$', IndexView.as_view(), name='index'),  # 显示主页
+    url(r'^index/$', IndexView.as_view(), name='index'),  # 显示主页
 
 ]
